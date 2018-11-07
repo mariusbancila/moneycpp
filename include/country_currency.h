7c52af3140bb96a20b620c5ad5c38d9b83a35632
Marius Bancila <marius.bancila@gmail.com>
2018-11-07 23:54:41 +0200

    country courencies C-H

--- include/country_currency.h
+++ include/country_currency.h
@@ -60,6 +60,89 @@ namespace moneycpp
          {BF, XOF},  // Burkina Faso
          {BI, BIF},  // Burundi
 
+                     // C
+         {CV, CVE},  // Cabo Verde
+         {KH, KHR},  // Cambodia
+         {CM, XAF},  // Cameroon
+         {CA, CAD},  // Canada
+         {KY, KYD},  // Cayman Islands (UK)
+         {CF, XAF},  // Central African Republic
+         {TD, XAF},  // Chad
+         {CL, CLP},  // Chile
+         {CL, CLF},  // Chile
+         {CN, CNY},  // China
+         {CX, AUD},  // Christmas Island (Australia)
+         {CC, AUD},  // Cocos (Keeling) Islands (Australia)
+         {CO, COP},  // Colombia
+         {CO, COU},  // Colombia
+         {KM, KMF},  // Comoros
+         {CD, CDF},  // Congo, Democratic Republic of the
+         {CG, XAF},  // Congo, Republic of the
+         {CK, NZD},  // Cook Islands (New Zealand)
+         {CR, CRC},  // Costa Rica
+         {CI, XOF},  // Cote d'Ivoire
+         {HR, HRK},  // Croatia
+         {CU, CUP},  // Cuba
+         {CU, CUC},  // Cuba
+         {CW, ANG},  // Curacao (Netherlands)
+         {CY, EUR},  // Cyprus
+         {CZ, CZK},  // Czech Republic
+
+                     // D
+         {DK, DKK},  // Denmark
+         {DJ, DJF},  // Djibouti
+         {DM, XCD},  // Dominica
+         {DO, DOP},  // Dominican Republic
+
+                     // E
+         {EC, USD},  // Ecuador
+         {EG, EGP},  // Egypt
+         {SV, SVC},  // El Salvador
+         {SV, USD},  // El Salvador
+         {GQ, XAF},  // Equatorial Guinea
+         {ER, ERN},  // Eritrea
+         {EE, EUR},  // Estonia
+         {SZ, SZL},  // Eswatini (formerly Swaziland)
+         {ET, ETB},  // Ethiopia
+
+                     // F
+         {FK, FKP},  // Falkland Islands (UK)
+         {FO, DKK},  // Faroe Islands (Denmark)
+         {FJ, FJD},  // Fiji
+         {FI, EUR},  // Finland
+         {FR, EUR},  // France
+         {GF, EUR},  // French Guiana (France)
+         {PF, XPF},  // French Polynesia (France)
+         {TF, EUR},  // French Southern Territories (France)
+
+                     // G
+         {GA, XAF},  // Gabon
+         {GM, GMD},  // Gambia
+         {GE, GEL},  // Georgia
+         {DE, EUR},  // Germany
+         {GH, GHS},  // Ghana
+         {GI, GIP},  // Gibraltar (UK)
+         {GR, EUR},  // Greece
+         {GL, DKK},  // Greenland (Denmark)
+         {GD, XCD},  // Grenada
+         {GP, EUR},  // Guadeloupe (France)
+         {GU, USD},  // Guam (USA)
+         {GT, GTQ},  // Guatemala
+         {GG, GBP},  // Guernsey (UK)
+         {GG, GGP},  // Guernsey (UK)
+         {GN, GNF},  // Guinea
+         {GW, XOF},  // Guinea-Bissau
+         {GY, GYD},  // Guyana
+
+                     // H
+         {HT, HTG},  // Haiti
+         {HT, USD},  // Haiti
+         {HM, AUD},  // Heard Island and McDonald Islands
+         {VA, EUR},  // Holy See
+         {HN, HNL},  // Honduras
+         {HK, HKD},  // Hong Kong (China)
+         {HU, HUF},  // Hungary
+
          //{, },  // 
       };
       
